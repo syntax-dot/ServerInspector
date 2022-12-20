9e8092230ab12b7349b6c9e274ae30d67db67c6a
Aleksandr <75541723+SHKUROLOG@users.noreply.github.com>
2022-12-20 17:38:14 +0300

    wip

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ function useBatRunner() {
   const id = (Math.random() * 10).toFixed(0);
 
   function start() {
+    // 1 TODO try/catch
+    // 3 Отслеживание с stdin TPS при снижении ниже порога нужно stop()
     try {
       currentBat = spawn('cmd.exe', ['/c', 'start.bat'], { cwd: './minecraft_server.1.12.2' });
       
@@ -46,6 +48,10 @@ function useBatRunner() {
   }
 
   function stop() {
+    // 2 TODO останавливает сервер
+    // 2 командой в stdin 'stop'
+    // 2 Если через минуту он не стопнулся, мягко кильнуть
+    // 2 Если через n сек он не стопнулся, кильнуть SIGKILL | SIGTERM
     currentBat.stdout.destroy();
     currentBat.stderr.destroy();
 
